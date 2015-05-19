b1e54e554d366a4b9181774a56fe605f8aaa1d0b
Piotr Jaworski <piotr.m.jaworski@gmail.com>
2015-05-19 14:48:30 +0200

    v.0.1.1
    Minot tweaks.

--- Rome/App_Start/BundleConfig.cs
+++ Rome/App_Start/BundleConfig.cs
@@ -15,10 +15,15 @@ namespace Rome
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/Scripts/angular.js"));
+                        "~/Scripts/angular.js",
+                        "~/Scripts/angular-animate.js",
+                        "~/Scripts/angular-aria.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                         "~/Scripts/app/app.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/material").Include(
+                        "~/Scripts/angular-material.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
--- Rome/Scripts/app/app.js
+++ Rome/Scripts/app/app.js
@@ -1,4 +1,17 @@
-﻿var myApp = angular.module('myApp', []);
+﻿var myApp = angular.module('myApp', ['ngMaterial']);
+
+myApp.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('light-blue', {
+        'default': '400',
+        'hue-1': '100',
+        'hue-2': '600',
+        'hue-3': 'A100'
+    })
+    .accentPalette('orange', {
+        'default': '200'
+    });
+});
 
 myApp.controller('mainCtrl', function ($scope, $http) {
     $scope.loading = true;
